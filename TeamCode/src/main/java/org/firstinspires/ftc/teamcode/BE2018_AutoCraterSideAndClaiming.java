754e3ab91903e5675e2244d06ebfd38dc271c5ea
Joshua Biggs <chiefjosh@gmail.com>
2019-01-12 12:12:12 -0700

    Revert "Revert "its 3:30am i am tired but look what I did pretty rad amirite?""
    This reverts commit 7ebab070432c7dc6368addecc1049fe8659541d6.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BE2018_AutoCraterSideAndClaiming.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BE2018_AutoCraterSideAndClaiming.java
@@ -5,39 +5,35 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.phases.AutonomousPhase;
 import org.firstinspires.ftc.teamcode.phases.DoNothingPhase;
 import org.firstinspires.ftc.teamcode.phases.DropRobotPhase;
-import org.firstinspires.ftc.teamcode.phases.ForwardColorCraterPhase;
+import org.firstinspires.ftc.teamcode.phases.ForwardColorCraterAndClaimingPhase;
 import org.firstinspires.ftc.teamcode.phases.ForwardDurationPhase;
 import org.firstinspires.ftc.teamcode.phases.ForwardSensePhase;
 
 import java.util.LinkedList;
 
-@Autonomous(name="BE2018_AutoCraterSide", group="Iterative Opmode")
-public class BE2018_AutoCraterSide extends AutoAbstract {
+@Autonomous(name="BE2018_AutoCraterSideAndClaiming", group="Iterative Opmode")
+public class BE2018_AutoCraterSideAndClaiming extends AutoAbstract {
 
     @Override
     void setPhases(LinkedList<AutonomousPhase> phaseList) {
         phaseList.add(new DropRobotPhase());
 
         // escape latch
-        phaseList.add(new ForwardDurationPhase(60, 0.4, true));
-        phaseList.add(new ForwardDurationPhase(60, 0.4, false));
-        phaseList.add(new ForwardDurationPhase(60, -0.4, true));
+        phaseList.add(new ForwardDurationPhase(70, 0.30, true));
+        phaseList.add(new ForwardDurationPhase(70, 0.30, false));
+        phaseList.add(new ForwardDurationPhase(70, -0.30, true));
 
         // forward to minerals
-        phaseList.add(new ForwardSensePhase(3.75, 1, false, 15, 0.98));
+        phaseList.add(new ForwardSensePhase(3.75, 1, false, 15, 0.90));
 
         // strafe left toward leftmost mineral
-        phaseList.add(new ForwardDurationPhase(200, 1, true));
+        phaseList.add(new ForwardDurationPhase(200, 0.5, true));
         //strafe to left most mineral
-        phaseList.add(new ForwardSensePhase(5, 0.30,true, 0, 0));
+        phaseList.add(new ForwardSensePhase(5, 0.25,true, 0, 0));
         //strafe past leftmost mineral
         phaseList.add(new ForwardDurationPhase(10, 0.5, true));
         //straft to gold mineral
-        phaseList.add(new ForwardColorCraterPhase(-0.30, true,50,10, 10000, 9));
-        //push gold mineral
-        phaseList.add(new ForwardDurationPhase(150, 1, false));
-        phaseList.add(new ForwardDurationPhase(300, -1, false));
-        phaseList.add(new ForwardDurationPhase(550, 1, true));
+        phaseList.add(new ForwardColorCraterAndClaimingPhase(-0.28, true,50,10, 10000, 9));
         phaseList.add(new DoNothingPhase());
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BE2018_AutoDepotSideWithParking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BE2018_AutoDepotSideWithParking.java
@@ -5,24 +5,24 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.phases.AutonomousPhase;
 import org.firstinspires.ftc.teamcode.phases.DoNothingPhase;
 import org.firstinspires.ftc.teamcode.phases.DropRobotPhase;
-import org.firstinspires.ftc.teamcode.phases.ForwardColorDepotPhase;
+import org.firstinspires.ftc.teamcode.phases.ForwardColorDepotWithParkingPhase;
 import org.firstinspires.ftc.teamcode.phases.ForwardDurationPhase;
 import org.firstinspires.ftc.teamcode.phases.ForwardSensePhase;
 import org.firstinspires.ftc.teamcode.phases.MarkerDropPhase;
 
 import java.util.LinkedList;
 
-@Autonomous(name="BE2018_AutoDepotSide", group="Iterative Opmode")
-public class BE2018_AutoDepotSide extends AutoAbstract {
+@Autonomous(name="BE2018_AutoDepotSideWithParking", group="Iterative Opmode")
+public class BE2018_AutoDepotSideWithParking extends AutoAbstract {
 
     @Override
     void setPhases(LinkedList<AutonomousPhase> phaseList) {
         phaseList.add(new DropRobotPhase());
 
         // escape latch
-        phaseList.add(new ForwardDurationPhase(100, 0.28, true));
-        phaseList.add(new ForwardDurationPhase(100, 0.28, false));
-        phaseList.add(new ForwardDurationPhase(100, -0.28, true));
+        phaseList.add(new ForwardDurationPhase(50, 0.40, true));
+        phaseList.add(new ForwardDurationPhase(50, 0.40, false));
+        phaseList.add(new ForwardDurationPhase(50, -0.40, true));
 
         // forward to minerals
         phaseList.add(new ForwardSensePhase(3.75, 1, false, 15, 0.98));
@@ -31,16 +31,15 @@ public class BE2018_AutoDepotSide extends AutoAbstract {
         phaseList.add(new ForwardDurationPhase(400, 0.5, true));
 
         // strafe to leftmost mineral
-        phaseList.add(new ForwardSensePhase(5, 0.25,true, 0, 0));
+        phaseList.add(new ForwardSensePhase(5, 0.34,true, 0, 0));
 
         //strafe past leftmost mineral
         phaseList.add(new ForwardDurationPhase(200, 0.5, true));
 
         // strafe to select gold mineral
-        phaseList.add(new ForwardColorDepotPhase(-0.28, true,60,10, 10000, 9));
+        phaseList.add(new ForwardColorDepotWithParkingPhase(-0.34, true,50,10, 10000, 9));
 
-        // Drop Marker
-        phaseList.add(new MarkerDropPhase(0.87, true));
+
 
         // Do Nothing
         phaseList.add(new DoNothingPhase());
